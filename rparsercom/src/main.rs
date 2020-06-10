928512f0222aef987045f29645456f0b8bc365d6
Stefan Wong <stfnwong@gmail.com>
2020-06-10 23:19:50 +0100

    Added the_letter_a() parser

--- rparsercom/src/main.rs
+++ rparsercom/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+/*
+ * Rust parser combinator 
+ * From here (https://bodil.lol/parser-combinators/)
+ */
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Element 
+{
+    name: String,
+    attributes: Vec<(String, String)>,
+    children: Vec<Element>
+}
+
+
+// parser whic can parse just the letter 'a'
+fn the_letter_a(input: &str) -> Result<(&str, ()), &str>
+{
+    match input.chars().next()
+    {
+        Some('a') => Ok((&input['a'.len_utf8()..], ())),
+        _ => Err(input),
+    }
+}
+
+
+fn main()
+{
+    let input = "aaab";
+    let res = the_letter_a(input);
+    println!("Input : {}", input);
+    //println!("First call to the_letter_a()");
+    println!("{:?}", res);
 }
